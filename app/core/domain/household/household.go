37715c8f505e3581c3fe618d5874d705da8f2824
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-13 23:55:09 -0300

    feat: Household

---
+++ app/core/domain/household/household.go
@@ -0,0 +1,16 @@
+package households
+
+import (
+	cats "kitten-backend-go-app/app/core/domain/cat"
+	users "kitten-backend-go-app/app/core/domain/user"
+)
+
+type Household struct {
+	ID        string
+	Name      string
+	AdminID   string
+	Members   []*users.User
+	Cats      []*cats.Cat
+	Latitude  float32
+	Longitude float32
+}
