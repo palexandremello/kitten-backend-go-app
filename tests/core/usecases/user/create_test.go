b40a98e4f34728c29488893eeae4d8deb3b7545d
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-17 03:44:46 -0300

    tests: ensure CreateUser usecase should create user successfully

---
+++ tests/core/usecases/user/create_test.go
@@ -0,0 +1,50 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/mock"
+
+	users "kitten-backend-go-app/app/core/domain/user"
+	"kitten-backend-go-app/app/core/usecases/user"
+)
+
+// RepositoryMock mock interface
+type RepositoryMock struct {
+	mock.Mock
+}
+
+func (m *RepositoryMock) Save(user *users.User) error {
+	args := m.Called(user)
+	return args.Error(0)
+}
+
+func (m *RepositoryMock) GetByEmail(email string) (*users.User, error) {
+	args := m.Called(email)
+	return nil, args.Error(1)
+}
+
+func TestCreateUser(t *testing.T) {
+	t.Run("should create user successfully", func(t *testing.T) {
+		mockRepo := new(RepositoryMock)
+		mockUser := &users.User{
+			Name:     "John",
+			Email:    "john@example.com",
+			Password: "password",
+		}
+		mockRepo.On("GetByEmail", mock.AnythingOfType("string")).Return(nil, nil)
+		mockRepo.On("Save", mock.AnythingOfType("*users.User")).Return(nil)
+
+		t.Log(mockUser)
+		usecase := user.NewUserService(mockRepo)
+		err := usecase.CreateUser(mockUser.Name, mockUser.Email, mockUser.Password)
+
+		mockRepo.AssertCalled(t, "Save", mock.AnythingOfType("*users.User"))
+
+		assert.NoError(t, err)
+		mockRepo.AssertExpectations(t)
+	})
+
+}
