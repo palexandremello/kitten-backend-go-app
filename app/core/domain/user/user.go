896c0da3cf4869ebd5c8220459ffa46109cd2c8e
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-26 02:21:27 -0300

    refactor: Refactoring User entity to use ValueObjects inside fields

--- app/core/domain/user/user.go
+++ app/core/domain/user/user.go
@@ -3,7 +3,7 @@ package users
 // User is the entity about users in application
 type User struct {
 	ID       string
-	Name     string
-	Email    string
-	Password string
+	Name     *Name
+	Email    *Email
+	Password *Password
 }
