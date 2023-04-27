029ef5d522f2cc4fd2621803eee7c2fc03e0efc6
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-27 01:14:18 -0300

    tests: ensure Email ValueObject should return a new email validated

---
+++ app/core/domain/user/email_test.go
@@ -0,0 +1,33 @@
+package users
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type mockEmailValidator struct {
+	mock.Mock
+}
+
+func (m *mockEmailValidator) Validate(email string) error {
+	args := m.Called(email)
+	return args.Error(0)
+}
+
+func TestEmail(t *testing.T) {
+
+	t.Run("should return a new email validated", func(t *testing.T) {
+
+		email := "palexandremello@gmail.com"
+		validator := &mockEmailValidator{}
+		validator.On("Validate", email).Return(nil)
+
+		result, err := NewEmail(email, validator)
+
+		assert.Nil(t, err)
+		assert.Equal(t, email, result.Value())
+	})
+
+}
