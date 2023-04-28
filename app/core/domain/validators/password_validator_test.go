3895f5ab6e0126d395f5e8cd7a79457aead87d46
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-28 00:29:43 -0300

    tests: ensure PasswordValidator should return an error when missing uper case letter

--- app/core/domain/validators/password_validator_test.go
+++ app/core/domain/validators/password_validator_test.go
@@ -18,4 +18,12 @@ func TestPasswordValidator(t *testing.T) {
 		assert.EqualError(t, err, ErrPasswordTooShort.Error())
 
 	})
+
+	t.Run("should return an error when missing upper case letter", func(t *testing.T) {
+		password := "abc12345"
+
+		err := validator.Validate(password)
+
+		assert.EqualError(t, err, ErrPasswordMissingUpper.Error())
+	})
 }
