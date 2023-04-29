f5b99b3d567998066ab102fd411bd9807cb85e48
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-04-29 13:52:08 -0300

    tests: UserPicture should be able to return an error when image size is too large (more than 10MB)

--- app/core/domain/user/user_picture_test.go
+++ app/core/domain/user/user_picture_test.go
@@ -14,4 +14,13 @@ func TestUserPicture(t *testing.T) {
 
 		assert.Error(t, err)
 	})
+
+	t.Run("should return an error when image size is too large", func(t *testing.T) {
+
+		data := make([]byte, 11<<20) // 11MB
+
+		_, err := NewUserPicture(data, "image/jpeg", "any_path")
+
+		assert.Error(t, err)
+	})
 }
