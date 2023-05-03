371442f44d92100d64589e7f572142f3dd8155a4
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-05-03 01:01:22 -0300

    tests: ensure Birthday ValueObject should return an error if birthday date is on future

--- app/core/domain/cat/birthday_test.go
+++ app/core/domain/cat/birthday_test.go
@@ -19,4 +19,13 @@ func TestBirthday(t *testing.T) {
 		assert.Equal(t, "birthday date is required", err.Error())
 	})
 
+	t.Run("should return an error if birthday date is after now", func(t *testing.T) {
+		futureValue := time.Now().AddDate(1, 0, 0)
+		birthday, err := NewBirthday(futureValue)
+
+		assert.Error(t, err)
+		assert.Nil(t, birthday)
+		assert.Equal(t, "birthday date is invalid", err.Error())
+	})
+
 }
