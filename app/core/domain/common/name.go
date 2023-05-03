d9e15c17db1f32b6c77091aab1b1883bc842ec87
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-05-03 01:09:43 -0300

    refactor: Common entity folder with first ValueObject with common between two entities

---
+++ app/core/domain/common/name.go
@@ -0,0 +1,24 @@
+package common
+
+import "errors"
+
+type Name struct {
+	value string
+}
+
+func NewName(name string) (*Name, error) {
+
+	if name == "" {
+		return nil, errors.New("name is required")
+	}
+
+	if len(name) > 100 {
+		return nil, errors.New("name is too long")
+	}
+
+	return &Name{value: name}, nil
+}
+
+func (n *Name) Value() string {
+	return n.value
+}
---
+++ app/core/domain/common/name_test.go
@@ -0,0 +1,34 @@
+package common
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestName(t *testing.T) {
+
+	t.Run("should create a new name", func(t *testing.T) {
+
+		name, err := NewName("Paulo Mello")
+		assert.Nil(t, err)
+		assert.Equal(t, "Paulo Mello", name.Value())
+	})
+
+	t.Run("should return an error if name is empty", func(t *testing.T) {
+
+		_, err := NewName("")
+		assert.NotNil(t, err)
+		assert.Equal(t, "name is required", err.Error())
+	})
+
+	t.Run("should return an error if name is too long", func(t *testing.T) {
+
+		_, err := NewName("Paulo" + strings.Repeat("a", 100))
+
+		assert.NotNil(t, err)
+
+		assert.Equal(t, "name is too long", err.Error())
+	})
+}
